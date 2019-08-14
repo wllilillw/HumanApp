11096b95b28d3d5e7a16dac9da751c463836c714
ユーザー名 <kanokuda.1@gmail.com>
2019-08-14 22:27:54 +0900

    再提出。hobbyをHumanクラスの変数に。

--- app/src/main/java/jp/techacademy/okuda/kanji/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/okuda/kanji/kotlinlog/Human.kt
@@ -4,7 +4,9 @@ import android.util.Log
 
 
 open class Human: Animal,Thinkable {
-    constructor(name: String, age: Int,hobby: String) : super(name, age, hobby) {
+    var hobby:String
+    constructor(name: String, age: Int,hobby: String) : super(name, age){
+        this.hobby = hobby
     }
 
     override fun say() {
